63e8056d7526a10be2dc70e33c94d8fd69082806
RafaelYajima <Rafaelyajima2@gmail.com>
2023-09-23 11:15:40 -0300

    .

--- Exercicio1.py
+++ Exercicio1.py
@@ -1,1 +1,15 @@
 # Exercicio 1
+
+maximo = int(input("Digite o numero maximo: "))
+ordem = input("Qual a ordem que deseja imprimir o numero C/D: ")
+
+if ordem == 'C':
+    for numero in range(1, maximo + 1):
+        print(numero)
+elif ordem == 'D':
+    for numero in range(maximo, 0, -1):
+        print(numero)
+else:
+    print("Ordem invalida!")
+
+
---
+++ Exercicio2.py
@@ -0,0 +1,5 @@
+# Exercicio 2
+
+tabuada = int(input("Tabuada do numero: "))
+for numero in range(0, 10, 2):
+    print 
